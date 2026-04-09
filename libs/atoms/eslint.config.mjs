39ce429fafb2f449d2d03bc493683516d656a30c
Jonny Wright <jonathon.wright@kerv.com>
2026-04-09 15:28:03 +0100

    fix: ignore storybook files in linting

--- libs/atoms/eslint.config.mjs
+++ libs/atoms/eslint.config.mjs
@@ -1,11 +1,19 @@
-import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+import nx from "@nx/eslint-plugin";
+import baseConfig from "../../eslint.config.mjs";
 
 export default [
-  ...nx.configs['flat/react'],
+  ...nx.configs["flat/react"],
   ...baseConfig,
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    ignores: [
+      "storybook-static/**",
+      "node_modules/**",
+      ".cache/**",
+      ".vite-temp/**",
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     // Override or add rules here
     rules: {},
   },
